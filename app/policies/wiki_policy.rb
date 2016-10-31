6ef8cab004680a6a76f886a2defb1a5cfa9a330d
Brian Mont <briansmont830@gmail.com>
2016-10-31 22:43:31 +0000

    wikis/edit shows dropdown menu for available collaborators. collaborator.rb model has variables and method determining who is still available

--- app/policies/wiki_policy.rb
+++ app/policies/wiki_policy.rb
@@ -42,10 +42,10 @@ class WikiPolicy < ApplicationPolicy
       wikis = []
       if user.role == 'admin'
         wikis = scope.all # if the user is an admin, show them all the wikis
-      elsif user.role == 'premium'
+      elsif user.role == 'premium_member'
         all_wikis = scope.all
         all_wikis.each do |wiki|
-          if !wiki.private? || wiki.owner == user || wiki.collaborators.include?(user)
+          if !wiki.private? || wiki.user_id == user.id || wiki.collaborators.include?(user)
             wikis << wiki # if the user is premium, only show them public wikis, or that private wikis they created, or private wikis they are a collaborator on
           end
         end
