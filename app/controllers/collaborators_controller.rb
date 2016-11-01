3d4c27dd6a7644a92e4ed0b5f9cbf66ac0c8967f
Brian Mont <briansmont830@gmail.com>
2016-11-01 20:25:55 +0000

    Able to add a collaborator, but when selecting delete, it deletes wiki rather than the collaborator - need to fix routing causing the problem

--- app/controllers/collaborators_controller.rb
+++ app/controllers/collaborators_controller.rb
@@ -1,2 +1,42 @@
 class CollaboratorsController < ApplicationController
+  
+  
+  
+  def new
+    @user = User.find(params[:user_id])
+    @wiki = Wiki.find(params[:wiki_id])
+    @collaborator = Collaborator.new
+  end
+  
+  def create
+    @collaborator = Collaborator.new(collaborator_params)
+    @wiki = Wiki.find_by(id: params[:collaborator][:wiki_id])
+    @user = User.find(params[:collaborator][:user_id])
+    if @collaborator.save
+      flash[:notice] = "You have added this collaborator"
+      redirect_to @wiki
+    else
+      flash[:error] = "There was an error adding the collaborator, please retry"
+      render :show
+    end
+  end
+  
+  def destroy
+    @wiki = Wiki.find(params[:wiki_id])
+    @collaborator = Collaborator.find(params[:id])
+    
+    if @collaborator.destroy
+      flash[:notice] = "You have removed this collaborator"
+      redirect_to @wiki
+    else
+      flash[:error] = "There was an error removing the collaborator, please retry"
+      render :show
+    end
+  end
+  
+  private
+  def collaborator_params
+    params.require(:collaborator).permit(:user_id, :wiki_id)
+  end
+
 end
