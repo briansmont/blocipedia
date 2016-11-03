b2d69d1f75ea945245c98c4c35c2691953b432e4
Brian Mont <briansmont830@gmail.com>
2016-11-03 23:15:51 +0000

    herokutrials config for production modified

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,6 +76,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  config.action_mailer.default_url_options = { host: 'https://git.heroku.com/limitless-tundra-40446.git' }
+  config.action_mailer.default_url_options = { host: 'https://git.heroku.com/limitless-tundra-40446.git/users/confirmation?' }
 
 end
